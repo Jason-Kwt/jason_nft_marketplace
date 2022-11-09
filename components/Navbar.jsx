8397d2ae30a3d41a48a78ed9e409b91ab110ec14
Jason Kong <jasonxkong@icloud.com>
2022-11-09 18:31:05 +0800

    navbar right side menu and button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,11 +6,65 @@ import Link from 'next/link';
 
 import images from '../assets';
 
+import Button from './Button';
+
+const menuitem = ['Explore NFTs', 'Listed NFTs', 'My NFTs'];
+
+const MenuItems = ({ isMobile, active, setActive }) => {
+  const generateLink = () => {};
+  return (
+    <ul
+      className={`list-none flexCenter flex-row ${
+        isMobile && 'flex-col h-full'
+      }`}
+    >
+      {menuitem.map((item, index) => (
+        <li
+          key={index}
+          onClick={() => {}}
+          className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 
+          ${
+            active === item
+              ? 'dark:text-white text-nft-black-1'
+              : 'dark:text-nft-gray-3 text-nft-gray-2'
+          }`}
+        >
+          <Link href="">{item}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const ButtonGroup = ({ setActive, router }) => {
+  const hasConnected = true;
+
+  return hasConnected ? (
+    <Button
+      btnName="Create"
+      classStyles="mx-2 rounded-xl"
+      handleClick={() => {
+        setActive('');
+        router.push('/create-nft');
+      }}
+    />
+  ) : (
+    <Button
+      btnName="Connect"
+      classStyles="mx-2 rounded-xl"
+      handleClick={() => {}}
+    />
+  );
+};
+
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
+  const router = useRouter();
+  const [active, setActive] = useState('Explore NFTs');
 
   return (
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
+      {/* Left-side Logo part */}
       <div className="flex flex-1 flex-row justify-start">
         <Link href="/">
           <div
@@ -42,14 +96,14 @@ const Navbar = () => {
           </div>
         </Link>
       </div>
-
+      {/* right-side navbar menu & button part */}
       <div className="flex flex-initial flex-row justify-end">
         <div className="flex items-center mr-2">
           <input
             type="checkbox"
             className="checkbox"
             id="checkbox"
-            onChange={() => {}}
+            onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
           />
           <label
             htmlFor="checkbox"
@@ -59,6 +113,15 @@ const Navbar = () => {
             <i className="fas fa-moon" />
             <div className="w-3 h-3 absolute bg-white rounded-full ball" />
           </label>
+        </div>
+
+        <div className="md:hidden flex">
+          <ul className="list-none flexCenter flex-row">
+            <MenuItems active={active} setActive={setActive} />
+            <div className="ml-4">
+              <ButtonGroup setActive={setActive} router={router} />
+            </div>
+          </ul>
         </div>
       </div>
     </nav>
